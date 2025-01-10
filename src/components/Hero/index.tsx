ba35f37e188ef4fc830e01949e3f138c87331727
Thanh Trinh <86676350+sythinh@users.noreply.github.com>
2025-01-10 10:31:39 +0700

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,12 +2,14 @@
 import { useState, useEffect } from "react";
 import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 // component
 import { Input } from "@headlessui/react";
 
 // icons
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
@@ -22,6 +24,33 @@ const Hero = () => {
     minutes: 0,
     seconds: 0,
   });
+  const t = useTranslations("Hero");
+
+  const [scrollDirection, setScrollDirection] = useState(null);
+  const [lastScrollPosition, setLastScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPosition = window.scrollY;
+
+      if (currentScrollPosition > lastScrollPosition) {
+        setScrollDirection("down");
+      } else if (currentScrollPosition < lastScrollPosition) {
+        setScrollDirection("up");
+      }
+
+      setLastScrollPosition(
+        currentScrollPosition <= 0 ? 0 : currentScrollPosition
+      );
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup event listener on unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollPosition]);
 
   useEffect(() => {
     const targetDate = new Date();
@@ -47,7 +76,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="relative w-full h-full md:h-screen flex flex-col-reverse  justify-center items-center text-center text-white bg-hero-bg-mobile md:bg-hero-bg-desktop bg-cover bg-center bg-no-repeat mx-auto px-4">
+    <div className="relative w-full h-full md:h-svh flex flex-col-reverse  justify-center items-center text-center text-white bg-hero-bg-mobile md:bg-hero-bg-desktop bg-cover bg-center bg-no-repeat mx-auto px-4">
       <div className="md:absolute md:z-0 md:left-0 ">
         <Image
           width={900}
@@ -58,8 +87,8 @@ const Hero = () => {
         />
       </div>
       <div className="flex flex-col w-full md:w-[756px] z-10 mt-20">
-        <h1 className="text-[40px] leading-[50px] md:text-6xl font-bold mt-6 mb-6 px-10 text-center">
-          We're Getting Ready
+        <h1 className="text-[40px] leading-[50px] md:text-7xl font-bold mt-6 mb-6 px-5 text-center">
+          {t("title")}
         </h1>
         <div className="md:px-20">
           <div className="flex items-center justify-center gap-4 md:gap-10 font-semibold text-black bg-white w-full rounded-lg px-10 py-5">
@@ -69,7 +98,7 @@ const Hero = () => {
               >
                 {timeLeft.days}
               </div>
-              <div className="text-base">Days</div>
+              <div className="text-base">{t("days")}</div>
             </div>
             <div>
               <span className="text-2xl">:</span>
@@ -80,7 +109,7 @@ const Hero = () => {
               >
                 {timeLeft.hours}
               </div>
-              <div className="text-base">Hours</div>
+              <div className="text-base">{t("hours")}</div>
             </div>
             <div>
               <span className="text-2xl">:</span>
@@ -91,7 +120,7 @@ const Hero = () => {
               >
                 {timeLeft.minutes}
               </div>
-              <div className="text-base">Minutes</div>
+              <div className="text-base">{t("minutes")}</div>
             </div>
             <div>
               <span className="text-2xl">:</span>
@@ -102,7 +131,7 @@ const Hero = () => {
               >
                 {timeLeft.seconds}
               </div>
-              <div className="text-base">Seconds</div>
+              <div className="text-base">{t("seconds")}</div>
             </div>
           </div>
         </div>
@@ -110,15 +139,14 @@ const Hero = () => {
         <div className="flex flex-col mt-6 w-full items-center">
           <div>
             <p className="mt-8 text-lg font-light md:px-32 mb-10">
-              We will back to something amazing. Getting the latest updates
-              about our games. Please sign up to our newsletter.
+              {t("description")}
             </p>
           </div>
 
           <div className="relative w-full md:w-4/6 text-center">
             <Input
               type="email"
-              placeholder="Enter your email"
+              placeholder={t("input")}
               className="px-4 py-2 rounded-l bg-white text-black rounded-md w-full"
             />
             <ArrowRightIcon
@@ -127,6 +155,13 @@ const Hero = () => {
             />
           </div>
         </div>
+        <button className="fixed bottom-1/2 right-8 md:right-10 -translate-y-1/2 bg-white text-black p-3 rounded-full shadow-md hover:bg-gray-600 hover:text-white transition-colors">
+          {scrollDirection === "down" ? (
+            <FaChevronUp width={40} height={40} />
+          ) : (
+            <FaChevronDown width={40} height={40} />
+          )}
+        </button>
       </div>
     </div>
   );
