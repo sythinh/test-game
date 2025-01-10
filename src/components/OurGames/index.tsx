62bcdd918439b4f041c640e2c79b247f42cacb71
Thanh Trinh <86676350+sythinh@users.noreply.github.com>
2025-01-10 10:31:57 +0700

    Update index.tsx

--- src/components/OurGames/index.tsx
+++ src/components/OurGames/index.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 // constants
 import { GAMES } from "@/constants";
+import { useTranslations } from "next-intl";
 
 const OurGames = () => {
+  const t = useTranslations("OurGames");
+
   return (
     <div className="container mx-auto py-6 px-4">
       <div className="text-center mb-10 ">
-        <h2 className="text-[40px] md:text-6xl font-bold">Our Games</h2>
+        <h2 className="text-[40px] md:text-6xl font-bold">{t("title")}</h2>
         <p className="text-gray-600 mt-2 md:w-2/4 mx-auto">
-          As a pioneer of mobile app gamification, we take pride in originality
-          and individuality, providing global players with state-of-the-art
-          games that feature splendid storylines, sensational sound effects and
-          magnificent animation that never cease to impress.
+          {t("description")}
         </p>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-0">
