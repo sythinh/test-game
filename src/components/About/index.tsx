3d5eadb6b6cd8eb8ed788b88b86159d762e31986
Thanh Trinh <86676350+sythinh@users.noreply.github.com>
2025-01-10 10:30:52 +0700

    Update index.tsx

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,10 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import React from "react";
 
 const AboutUs = () => {
+  const t = useTranslations("About");
+
   return (
     <section className="md:py-16 md:px-16 container mx-auto px-4">
       <div className="flex flex-col md:flex-row mx-auto w-full md:w-2/3">
@@ -9,18 +12,9 @@ const AboutUs = () => {
         <div className="flex flex-col w-full md:w-1/2 md:mr-10">
           <div className="mb-12 w-full">
             <h2 className="text-[40px] md:text-6xl font-bold text-gray-900">
-              About Us
+              {t("title")}
             </h2>
-            <p className="text-gray-600 mt-4">
-              Browse our selection of free online games and have a great time
-              without leaving the site! Our Kids Games option also includes game
-              reviews, extensive game cheats and walkthroughs, and much more. We
-              have exclusive free downloads, videos, and articles as well. Etech
-              reviews the most popular kids games from all the most popular
-              video gaming platforms, so you don’t need to search around for fun
-              anywhere else on the Internet. Explore a whole new world of gaming
-              on Etech.
-            </p>
+            <p className="text-gray-600 mt-4">{t("description")}</p>
           </div>
 
           {/* Stats Section */}
@@ -31,11 +25,11 @@ const AboutUs = () => {
                   600 +<p className="absolute left-24 top-1/2 text-lg">M</p>
                 </h3>
               </div>
-              <p className="text-black mt-2 font-bold">Users</p>
+              <p className="text-black mt-2 font-bold">{t("users")}</p>
             </div>
             <div className="md:mt-10">
               <h3 className="text-5xl font-extrabold text-blue-600">135+</h3>
-              <p className="ext-black font-bold mt-2">Games</p>
+              <p className="ext-black font-bold mt-2">{t("games")}</p>
             </div>
           </div>
         </div>
@@ -47,11 +41,10 @@ const AboutUs = () => {
               <Image src="/icons/time.svg" width={50} height={50} alt="time" />
             </div>
             <div>
-              <h4 className="text-xl font-bold text-gray-900 mb-2">24-Hour</h4>
-              <p className="text-gray-600">
-                24/7 access ensures operators' businesses run smoothly all year
-                long.
-              </p>
+              <h4 className="text-xl font-bold text-gray-900 mb-2">
+                {t("24h")}
+              </h4>
+              <p className="text-gray-600">{t("desc24h")}</p>
             </div>
           </div>
 
@@ -65,11 +58,10 @@ const AboutUs = () => {
               />
             </div>
             <div>
-              <h4 className="text-xl font-bold text-gray-900 mb-2">Design</h4>
-              <p className="text-gray-600">
-                Combining imaginative universes, play dynamics, and innovative
-                gameplay.
-              </p>
+              <h4 className="text-xl font-bold text-gray-900 mb-2">
+                {t("design")}
+              </h4>
+              <p className="text-gray-600">{t("descDesign")}</p>
             </div>
           </div>
 
@@ -78,11 +70,10 @@ const AboutUs = () => {
               <Image src="/icons/team.svg" width={50} height={50} alt="time" />
             </div>
             <div>
-              <h4 className="text-xl font-bold text-gray-900 mb-2">Team</h4>
-              <p className="text-gray-600">
-                An award-winning studio of designers, artists, and producers
-                creating content for the biggest names in gaming.
-              </p>
+              <h4 className="text-xl font-bold text-gray-900 mb-2">
+                {t("team")}
+              </h4>
+              <p className="text-gray-600">{t("descTeam")}</p>
             </div>
           </div>
         </div>
