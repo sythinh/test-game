56c1901c11d57dd1f9ea110c3da1ced78cb8c617
Thanh Trinh <86676350+sythinh@users.noreply.github.com>
2025-01-10 10:29:22 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
